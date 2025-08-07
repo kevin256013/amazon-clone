454b11e8438d71f18060494b97ffd6cbdb5e8bfb
Kevin Lema Romero <kevinblemaromero2000@gmail.com>
2025-08-07 17:47:28 -0400

    Setup fronend and configured firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAak0lRqVoF_iJt54rrlEdevVFWcBPIXYU",
+  authDomain: "clone-3ac13.firebaseapp.com",
+  projectId: "clone-3ac13",
+  storageBucket: "clone-3ac13.firebasestorage.app",
+  messagingSenderId: "998024067240",
+  appId: "1:998024067240:web:b1e78c41df6b338c766a9d",
+  measurementId: "G-GWKTP3YWSZ"
+};
